74ec70fb184a909bc80479c59e27d0190e100f6a
miguel.desktop <miguelfm.pro@gmail.com>
2021-02-14 12:19:06 +0800

    update js

--- apps.js
+++ apps.js
@@ -1,11 +1,23 @@
 const jsHeader = document.querySelector('header');
 const jsNavHamburger = document.querySelector('.nav--bar--hamburger');
 const jsNavBar_ul_sm = document.querySelector('.nav-bar--ul--sm');
+const min400MediaQuery = window.matchMedia('(min-width: 420px)');
 // jsNavHamburger.style.backgroundColor = 'red';
 
 
-
 jsNavHamburger.addEventListener('click',()=>{
-    jsNavBar_ul_sm.classList.toggle('show');
-    jsHeader.classList.toggle('show');
+    jsNavBar_ul_sm.classList.toggle('show'); // shows the nav link
+    jsHeader.classList.toggle('show'); // change the height to auto
 })
+
+let  checkCurrentSize = (matchMe) => {
+    if(matchMe.matches){
+        // this remove the class that was activated
+        jsNavBar_ul_sm.classList.remove('show');
+        jsHeader.classList.remove('show');
+    }
+
+}
+
+min400MediaQuery.addEventListener('change',checkCurrentSize)
+// checkCurrentSize(min400MediaQuery);
